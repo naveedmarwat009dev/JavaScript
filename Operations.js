5fcbb580b011d39b6df4980165c063b03fc2208e
Muhammad Naveed <naveedmarwat009@gmail.com>
2026-08-26 23:40:51 +0500

    Update JavaScript practice

--- Operations.js
+++ Operations.js
@@ -1,6 +1,13 @@
 let value= 3
 
 let negValue= -value
-console.log(negValue)
+
+console.log(2+2);
+console.log(2-2);
+console.log(2*2);
+console.log(2/2);
+console.log(2%2);
+console.log(2**2);
+console.log(22);
 
 
---
+++ combination.js
@@ -0,0 +1,6 @@
+let str1= "hello"
+let str2="hitesh"
+
+let str3= str1+str2
+
+console.log(str3);
