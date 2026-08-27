a89750b83550d8c2ce207510e60aaa51dbc53658
Muhammad Naveed <naveedmarwat009@gmail.com>
2026-08-27 22:35:17 +0500

    check

--- conditional.js
+++ conditional.js
@@ -4,5 +4,10 @@
 // console.log(2===2);
 // console.log(2!==2);
 
-console.log("2">1); // true
-console.log("02">1); // false
+// console.log("2">1); // true
+// console.log("02">1); // false
+
+// console.log(null > 0); // false
+// console.log(null == 0); // true 
+// console.log(null >= 0); // true
+
