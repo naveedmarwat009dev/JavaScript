bbacfc7fe1f113b35f8c820927c6c8b45e1c4d01
Muhammad Naveed <naveedmarwat009@gmail.com>
2026-08-27 18:28:07 +0500

    Conditional operators

--- combination.js
+++ combination.js
@@ -1,6 +1,12 @@
-let str1= "hello"
-let str2="hitesh"
+// let str1= "hello"
+// let str2=" hitesh"
 
-let str3= str1+str2
+// let str3= str1+str2
 
-console.log(str3);
+// console.log(str3);
+
+ console.log("1"+2);
+ console.log(1+"2");
+ console.log(1+2+"3");
+
+ console.log((3+2)*5%3);
