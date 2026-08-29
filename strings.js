88e5f623833be69d055d96c7b731d8ace0b68105
Muhammad Naveed <naveedmarwat009@gmail.com>
2026-08-29 21:34:59 +0500

    Methods of strings java script

---
+++ strings.js
@@ -0,0 +1,16 @@
+// // concatention of strings
+// let name="Naveed"
+// let channel="CodingwithNaveed"
+// let fullName=name+" "+channel
+// console.log(fullName);
+
+// // template strings
+// let newFullName=` Hello my name is ${name}  and I am a  ${channel}  creator`
+// console.log(newFullName);
+const gameNew= new String("FIFA 2024")
+console.log(gameNew.__proto__);
+
+console.log(gameNew.toUpperCase());
+console.log(gameNew.toLowerCase()); 
+console.log(gameNew.indexOf(2));
+
