07e97a93f406e55322fc207fb6c65e87d6c47e4c
Muhammad Naveed <naveedmarwat009@gmail.com>
2026-08-31 22:16:05 +0500

    precision in nums and maths

---
+++ nums-maths.js
@@ -0,0 +1,12 @@
+// const score= 400
+// console.log(score);
+
+const balance= new Number(100)
+console.log(balance);
+ 
+console.log(balance.toString());
+console.log(balance.toFixed(2));
+
+const otherNumber=23.7898
+
+console.log(otherNumber.toPrecision(3));
